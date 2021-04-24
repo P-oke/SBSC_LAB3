ef4ba81cb44ea8c35c999562e61aed0ee91d617a
Oke Paul <paulayomikun@gmail.com>
2021-04-24 20:25:01 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input your second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+
+            int result = firstNumber % secondNumber;
+
+            Console.WriteLine(result);
+
+
+        }
+
+
+    }
+}
